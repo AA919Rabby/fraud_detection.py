4eef1f1e927ada2532305cdd15aa9e576349ef85
AA919Rabby <roycerabbikhan999@gmail.com>
2026-08-30 00:01:45 +0600

    db

--- src/model_db.py
+++ src/model_db.py
@@ -1,16 +1,17 @@
-from sqlalchemy import Column,Integer,Float,Boolean,String,DateTime
+from sqlalchemy import Column, Integer, Float, Boolean, String, DateTime
 from sqlalchemy.sql import func
 from src.database import Base
 
 class TransactionLog(Base):
-    __tablename__="transaction_log"
-    id=Column(Integer,primary_key=True,index=True)
-    amount=Column(Float)
-    hour_of_day=Column(Integer)
-    is_new_device=Column(Integer)
-    account_age=Column(Float)
-    is_fraud=Column(Boolean)
-    fraud_probability=Column(Float)
-    risk_level=Column(String)
-    created_at=Column(DateTime(timezone=True),server_default=func.now())
+    __tablename__ = "transaction_logs"
 
+    id = Column(Integer, primary_key=True, index=True)
+    amount = Column(Float)
+    hour_of_day = Column(Float)
+    transactions_last_hour = Column(Integer)
+    is_new_device = Column(Integer)
+    account_age_days = Column(Float)
+    is_fraud = Column(Boolean)
+    fraud_probability = Column(Float)
+    risk_level = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
