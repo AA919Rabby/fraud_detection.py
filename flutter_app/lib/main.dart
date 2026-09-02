c6e3388f8173f09b5e5eb896d16def57606ee7cc
AA919Rabby <roycerabbikhan999@gmail.com>
2026-09-02 11:13:04 +0600

    jdbfjdbf

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'screens/auth_screen.dart';
 import 'screens/home_screen.dart';
 
@@ -29,24 +30,58 @@ class AuthGate extends StatefulWidget {
 class _AuthGateState extends State<AuthGate> {
   String? token;
   String? baseUrl;
+  bool checking = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSavedSession();
+  }
+
+  Future<void> _loadSavedSession() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      token = prefs.getString("token");
+      baseUrl = prefs.getString("baseUrl");
+      checking = false;
+    });
+  }
+
+  Future<void> _saveSession(String t, String url) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("token", t);
+    await prefs.setString("baseUrl", url);
+    setState(() {
+      token = t;
+      baseUrl = url;
+    });
+  }
+
+  Future<void> _clearSession() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove("token");
+    await prefs.remove("baseUrl");
+    setState(() {
+      token = null;
+      baseUrl = null;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (token == null || baseUrl == null) {
-      return AuthScreen(
-        onLoggedIn: (t, url) => setState(() {
-          token = t;
-          baseUrl = url;
-        }),
+    if (checking) {
+      return const Scaffold(
+        backgroundColor: Color(0xFF0F0C29),
+        body: Center(child: CircularProgressIndicator()),
       );
+    }
+    if (token == null || baseUrl == null) {
+      return AuthScreen(onLoggedIn: _saveSession);
     }
     return HomeScreen(
       token: token!,
       baseUrl: baseUrl!,
-      onLogout: () => setState(() {
-        token = null;
-        baseUrl = null;
-      }),
+      onLogout: _clearSession,
     );
   }
 }
